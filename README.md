51f7aadeab0734793746dee2cb26ff773d88417a
changhyeonnam <hj04143@gmail.com>
2022-01-21 19:13:16 +0900

    dataset downloads complete

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 This repoistory is about implentation of "Attention Is All You Need" in pytorch.
 
+
 ### reference
 1. [Attention is All You Need](https://arxiv.org/abs/1706.03762)
 2. [paper review written in korean](https://changhyeonnam.github.io/2022/01/20/attention-is-all-you-need.html) 
--- utils.py
+++ utils.py
@@ -8,17 +8,17 @@ def downloads(file_list,root_dir:str='datatset',file_size:str='small',):
     if file_size == 'small':
         url = 'https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-vi'
         dir = os.path.join(root_dir, file_size)
-        print('Dataset\'s is about WMT\'15 English-Czech data | data size\'s[small]')
+        print('Downloading Dataset\'s is about WMT\'15 English-Czech data | data size\'s[small]')
 
     elif file_size == 'medium':
         url = 'https://nlp.stanford.edu/projects/nmt/data/wmt14.en-de'
         dir = os.path.join(root_dir, file_size)
-        print('Dataset\'s is about WMT\'14 English-German data| data size\'s[medium]')
+        print('Downloading Dataset\'s is about WMT\'14 English-German data| data size\'s[medium]')
 
     elif file_size == 'large':
         url = 'https://nlp.stanford.edu/projects/nmt/data/iwslt15.en-v'
         dir = os.path.join(root_dir, file_size)
-        print('Dataset\'s is about WMT\'15 English-Czech | data size\'s[large]')
+        print('Downloading Dataset\'s is about WMT\'15 English-Czech | data size\'s[large]')
 
     if not os.path.exists(dir):
         os.makedirs(dir)
@@ -30,6 +30,7 @@ def downloads(file_list,root_dir:str='datatset',file_size:str='small',):
                 for chunck in req.iter_content(chunk_size=None):
                     fd.write(chunck)
 
+    print("Downloading Dataset complete")
 
 
 
